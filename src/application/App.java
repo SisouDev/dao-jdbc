24da0597f5189e15c97bb8efd187ca1e57b09121
Karolina <karolinasingulani2@gmail.com>
2023-10-18 15:23:20 -0300

    new DAO

--- src/application/App.java
+++ src/application/App.java
@@ -1,14 +1,13 @@
 package application;
 
-import java.util.Date;
-
-import models.entities.Department;
+import models.dao.DaoFactory;
+import models.dao.SellerDao;
 import models.entities.Seller;
 
 public class App {
     public static void main(String[] args){
-        Department department = new Department(1, "Books");
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+        SellerDao sellerDao = DaoFactory.creatSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
