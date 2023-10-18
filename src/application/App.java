348fd60f7c619cfd3bdb73fa3d2c1e9697de8308
Karolina <karolinasingulani2@gmail.com>
2023-10-18 14:37:03 -0300

    new classes

--- src/application/App.java
+++ src/application/App.java
@@ -1,8 +1,14 @@
 package application;
 
+import java.util.Date;
+
+import models.entities.Department;
+import models.entities.Seller;
 
 public class App {
     public static void main(String[] args){
-        
+        Department department = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+        System.out.println(seller);
     }
 }
