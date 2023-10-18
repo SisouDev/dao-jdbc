5bc37db9628cbbbc1e286c0a3175ffc607234271
Karolina <karolinasingulani2@gmail.com>
2023-10-18 14:45:36 -0300

    new interfaces

---
+++ src/models/dao/DepartmentDao.java
@@ -0,0 +1,13 @@
+package models.dao;
+
+import java.util.List;
+
+import models.entities.Department;
+
+public interface DepartmentDao {
+    void insert(Department department);
+    void update(Department department);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
