3c7508f92abc129c77e7a82bc926115486d81915
Karolina <karolinasingulani2@gmail.com>
2023-10-18 16:39:17 -0300

    Update

--- src/application/App.java
+++ src/application/App.java
@@ -18,14 +18,12 @@ public class App {
         System.out.println("\n=== TEST 2: seller findByDepartment ====");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
-
         for (Seller obj : list) {
             System.out.println(obj);
         }
 
         System.out.println("\n=== TEST 3: seller findAll ====");
         list = sellerDao.findAll();
-
         for (Seller obj : list) {
             System.out.println(obj);
         }
@@ -34,5 +32,12 @@ public class App {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New Id = " + newSeller.getId());
+
+
+        System.out.println("\n=== TEST 5: seller update ====");
+        seller = sellerDao.findById(7);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
